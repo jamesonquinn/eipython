94046aa22fa6f71ec72012bbe9f6372b9b3fc9d9
Jameson Quinn <jameson.quinn+github@gmail.com>
2019-02-15 17:15:04 -0500

    some work

--- scratch.py
+++ scratch.py
@@ -1,9 +1,17 @@
-print(mt(2,2,2,2)[2,2])
+print(mt(2,2,2,2)[1,1])
+def approx_eq(a,b):
+    print(torch.lt(torch.abs(torch.add(a, -b)), 1e-8))
+    print("So:",torch.all(torch.lt(torch.abs(torch.add(a, -b)), 1e-8)))
+    print("So2:",torch.all(torch.lt(torch.abs(torch.add(a, -b)), 1e-8))==
+            torch.all(torch.lt(zs(1),1)))
+    return(torch.all(torch.lt(torch.abs(torch.add(a, -b)), 1e-8)))
+
 def get_directions(R, C, rnums, cnums): #note, not-voting is a candidate
     assert len(rnums)==R
     assert len(cnums)==C
     tot = sum(rnums)
-    assert tot==sum(cnums)
+    print("sums",tot,sum(cnums))
+    assert approx_eq(tot,sum(cnums))
     indep = ts([[(rnum * cnum / tot) for cnum in cnums] for rnum in rnums])
     basis = mt(R,C,R,C) #the first R,C tells "which basis vector"; the second R,C hold the elements of that "basis vector"
     for rbas in range(R):
@@ -29,14 +37,30 @@ def inbasis(R, C, raw, basis):
     result = zs(R, C)
     for r in range(R):
         for c in range(C):
-            result.add_(raw[R,C] * basis[R,C]) #scalar times matrix
+            result.add_(raw[r,c] * basis[r,c]) #scalar times matrix
     return result
 
-def inbasis_pos(R, C, raw, basis, start):
+def polytopize(R, C, raw, basis, start):
     if 0==torch.max(torch.abs(raw)):
         return(start)
     step1 = inbasis(R, C, raw, basis)
     ratio = torch.div(step1, start)
     closest = torch.min(ratio)
     return((step1 / -closest) * (1 - exp(closest)))
-    
+
+def test_funs(R, C, innerReps=2, outerReps=2):
+    for i in range(outerReps):
+        rnums = pyro.distributions.Exponential(1.).sample(torch.Size([R]))
+        cnums = pyro.distributions.Exponential(1.).sample(torch.Size([C]))
+        cnums = cnums / sum(cnums) * sum(rnums)
+        indep, basis = get_directions(R,C,rnums,cnums)
+        for j in range(innerReps):
+            loc = pyro.distributions.Normal(0.,4.).sample(torch.Size([R,C]))
+            polytopedLoc = polytopize(R,C,loc,basis,indep)
+            assert rnums == sum(polytopedLoc, dim=1).view(R)
+            assert cnums == sum(polytopedLoc, dim=0).view(C)
+
+test_funs(2,2)
+test_funs(5,2)
+test_funs(2,4)
+test_funs(6,8)
