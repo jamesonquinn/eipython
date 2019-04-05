de01a04953f38472595b4c518f9e62f5c99fdbec
Jameson Quinn <jameson.quinn+github@gmail.com>
2019-04-05 11:57:45 -0400

    toypyro first-draft "complete"

--- toypyro.py
+++ toypyro.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 print('Yes, I will run.')
 
 import torch
@@ -14,12 +15,7 @@ pyro.set_rng_seed(0)
 
 # Suppose we have aggregate level counts of two classes of voters and of votes in each of P precincts. Let's generate some fake data.
 
-P = 20
-populations = torch.distributions.Gamma(2.0, 1e-2).sample((P,
-        2)).round()
-print 'populations = {}'.format(populations)
-assert populations.min() > 0, 'found a zero-sized population; try again'
-
+#...
 
 # Now suppose precinct-level behavior follows a Beta distribution with class-dependent parameters, so that individual level behavior is Bernoulli distributed. We can write this as a generative model that we'll use both for data generation and inference.
 
@@ -30,7 +26,7 @@ def model(data=None, include_nuisance=False):
         ns = torch.zeros(P,R)
         for p in range(P):
             for r in range(R):
-                ns[p,r] = pyro.sample('precinctSizes', dist.negative_binomial(n + r + 1, .95))
+                ns[p,r] = pyro.sample('precinctSizes', dist.NegativeBinomial(n + r + 1, .95))
     else:
         ns, vs = data
         assert len(ns)==len(vs)
@@ -44,10 +40,13 @@ def model(data=None, include_nuisance=False):
         ec = pyro.sample('ec', dist.Normal(0,sdc))
         with pyro.plate('rgroups', R):
             erc = pyro.sample('erc', dist.Normal(0,sdrc))
-            with pyro.plate('precincts', P):
-                eprc = pyro.sample('eprc', dist.Normal(0,sdprc))
+            if include_nuisance:
+                with pyro.plate('precincts', P):
+                    eprc = pyro.sample('eprc', dist.Normal(0,sdprc))
 
-    logittotals = ec+erc+eprc
+    logittotals = ec+erc
+    if include_nuisance:
+        logittotals += eprc
 
     y = zs(P,R,C)
 
@@ -56,7 +55,7 @@ def model(data=None, include_nuisance=False):
         tmp = torch.exp(logittotals[p,r])
         cprobs = tmp/torch.sum(tmp)
         n = int(ns[p,r].item())
-        samp= pyro.sample(f"y_{p}_{r}",
+        samp= pyro.sample(f"y_{p}",
                     dist.Multinomial(n,cprobs)
                     .to_event(1))
         y[p,r] = samp
@@ -72,19 +71,85 @@ def model(data=None, include_nuisance=False):
 # Let's generate voting data. I'm conditioning concentration=1 for numerical stability.
 
 data = model()
-print data
+print(data)
 
 # Let's now write a variational approximation.
 
 init_narrow = 10  # Numerically stabilize initialization.
 
-def guide(data):
+def guide(data, include_nuisance=False):
+
+    ns, vs = data
+    P = len(ns)
+    R = len(ns[1])
+    C = len(vs[1])
+    #Start with hats.
+
+    hat_data = dict()
     sdrchat = pyro.param('sdrchat',5)
     sdprchat = pyro.param('sdprchat',5)
+    hat_data.update(sdrc=sdrchat,sdprc=sdprchat)
     with pyro.plate('candidates', C):
         echat = pyro.param('echat', 0)
         with pyro.plate('rgroups', R):
             erchat = pyro.param('erchat', 0)
+            if include_nuisance:
+                with pyro.plate('precincts', P):
+                    eprchat = pyro.param('eprchat', 0)
+    hat_data.update(ec=echat,erc=erchat,eprc=eprchat)
+
+
+    with pyro.plate('precincts2', P):
+        with pyro.plate('Wcandidates', C):
+            with pyro.plate('Wrgroups', R):
+                what = pyro.param('what', 0)
+
+    for p in pyro.plate('precincts3', P):
+        indep = get_indep(R, C, ns[p], vs[p])
+        yhat = polytopize(R,C,indep,what[p])
+        yy = pyro.param(f"y_{p}_hat",
+                    yhat)
+        hat_data.update({f"y_{p}":yy})
+        if include_nuisance:
+            pass #unimplemented — get MLE for gamma, yuck
+
+
+    #Get hessians and sample params
+
+    #Start with theta
+
+    hessCenter = pyro.condition(model,hat_data)
+    trace1 = poutine.trace(hessCenter)
+    trace2 = trace1.get_trace(data, include_nuisance)
+    loss = -trace2.log_prob_sum()
+    thetaParts = [sdrchat, sdprchat, echat, erchat]
+    H = hessian.hessian(loss, thetaParts)#, allow_unused=True)
+
+    thetaMean = thetaParts.view(-1)
+    tlen = len(thetaMean)
+    theta = pyro.sample('theta',
+                    dist.MultivariateNormal(thetaMean, precision_matrix=H),
+                    infer={'is_auxiliary': True})
+
+
+    for p in pyro.plate('precincts3', P):
+        wthetaparts = thetaParts + [what[p]]
+        HW = hessian.hessian(loss, wthetaParts)
+        Sig = torch.inverse(H) #This is not efficient computationally — redundancy. But I don't want to hand-code the right thing yet.
+        wmean = (what[p].view(-1) +
+                Sig[tlen:, :tlen] * H * (theta - thetaMean))
+        wSig = Sig[tlen:, tlen:] - Sig[tlen:, :tlen] * H * Sig[:tlen, tlen:]
+        w = pyro.sample(f"w_{p}",
+                        dist.MultivariateNormal(wmean, wSig),
+                        infer={'is_auxiliary': True})
+        indep = get_indep(R, C, ns[p], vs[p])
+        y = polytopize(R,C,indep,w.view(R,C))
+        yy = pyro.sample(f"y_{p}", dist.Delta(y))
+        if include_nuisance:
+            pass #unimplemented — gamma
+
+
+
 
 def fritzguide(populations, data):
 
@@ -133,7 +198,7 @@ for i in range(3001):
     loss = svi.step(populations, data)
     losses.append(loss)
     if i % 100 == 0:
-        print 'epoch {} loss = {}'.format(i, loss)
+        print('epoch {} loss = {}'.format(i, loss))
 
 ##
 
@@ -144,4 +209,4 @@ plt.ylabel('loss')
 ##
 
 for (key, val) in sorted(pyro.get_param_store().items()):
-    print '{}:\n{}'.format(key, val)
+    print(f"{key}:\n{val}")
