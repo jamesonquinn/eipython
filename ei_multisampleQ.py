fix variance?? by less boosting

ei_multisampleQ.py
@@ -53,7 +53,7 @@ pyro.enable_validation(True)
 pyro.set_rng_seed(0)
 
 
-EI_VERSION = "4.0.0"
+EI_VERSION = "5.0.0"
 FILEBASE = "ei_post_results_fixedalpha/"
 init_narrow = 10  # Numerically stabilize initialization.
 
@@ -76,7 +76,7 @@ SDS_TO_SHRINK_BY = 2/3 #neutral = 1.
 REATTACH_GRAD_PORTION = 1. #neutral = 1.
 SIGMA_NU_DETACHED_FRACTION = 1. #Neutral = 0. but very defensible up to 1. Moreover, seems to work!
 
-NSTEPS = 2000
+NSTEPS = 2#000
 SUBSET_SIZE = 30
 #BIG_PRIME = 73 #Wow, that's big!
 
@@ -84,6 +84,7 @@ FAKE_VOTERS_PER_RACE = 1.
 FAKE_VOTERS_PER_REAL_PARTY = .5 #remainder go into nonvoting party
 
 BASE_PSI = .01
+FIT_PSIS = False
 
 QUICKIE_SAVE = (NSTEPS < 20) #save subset; faster
 CUTOFF_WINDOW = 140
@@ -850,13 +851,19 @@ def guide(data, scale, include_nuisance=True, do_print=False, inits=dict(), nsam
     # Psis
     ##################################################################
     #declare global-level psi params
-    globalpsi = get_param(inits,'globalpsi',torch.ones(gamma_dims)*BASE_PSI,
-                constraint=constraints.positive,
-                as_constant=PIN_PSI)
-    #declare precinct-level psi params
-    precinctpsi = get_param(inits,'precinctpsi',BASE_PSI * torch.ones(dims_per_unit),
-                constraint=constraints.positive,
-                as_constant=PIN_PSI)
+    if FIT_PSIS:
+        globalpsi = get_param(inits,'globalpsi',torch.ones(gamma_dims)*BASE_PSI,
+                    constraint=constraints.positive,
+                    as_constant=PIN_PSI)
+        #declare precinct-level psi params
+        precinctpsi = get_param(inits,'precinctpsi',BASE_PSI * torch.ones(dims_per_unit),
+                    constraint=constraints.positive,
+                    as_constant=PIN_PSI)
+    else:
+        globalpsi = torch.ones(gamma_dims)*BASE_PSI
+        #declare precinct-level psi params
+        precinctpsi = BASE_PSI * torch.ones(dims_per_unit)
+
 
     #dp"setpsis",sizes(globalpsi,precinctpsi))
     big_arrow.setpsis(globalpsi,precinctpsi)
@@ -1360,6 +1367,7 @@ def sampleYs(fit,data,n,previousSamps = None,weightToUndo=1.,indices=None, icky_
 
     if previousSamps is None:
         YSums = torch.zeros(n,R,C)
+        meanYSums = torch.zeros(n,R,C)
         ldajsums = torch.zeros(n)
         guidesampdenses = torch.zeros(n)
         moddenses = torch.zeros(n)
@@ -1379,7 +1387,7 @@ def sampleYs(fit,data,n,previousSamps = None,weightToUndo=1.,indices=None, icky_
 
 
     else:
-        gammas,YSums, stuff, Qvarcounters = previousSamps
+        gammas,YSums, meanYSums,stuff, Qvarcounters = previousSamps
         guidesampdenses,ldajsums,moddenses,modnps = [stuff[:,i] for i in range(4)]
         dp("sampleYs0",sizes(gammas,YSums, stuff,guidesampdenses,ldajsums,moddenses,modnps))
         #import pdb; pdb.set_trace()
@@ -1406,8 +1414,10 @@ def sampleYs(fit,data,n,previousSamps = None,weightToUndo=1.,indices=None, icky_
 
         guidesampdenses += llp
         ws = lambdas[:,:wdim].view(n,R-1,C-1)
+        wmeans = lmean[:,:wdim].view(n,R-1,C-1)
         #import pdb; pdb.set_trace()
         ys,ldajs = polytopizeU(R, C, ws, data.indeps[u:u+1].expand(n,R*C), return_ldaj=True, return_plural=True)
+        meanys = polytopizeU(R, C, wmeans, data.indeps[u:u+1].expand(n,R*C), return_ldaj=False, return_plural=True)
         if icky_sigma:
             sigma_nu = fit["fstar_data"]["sdprc"].unsqueeze(0)
             dp("sdprc",sizes(sigma_nu))
@@ -1419,6 +1429,7 @@ def sampleYs(fit,data,n,previousSamps = None,weightToUndo=1.,indices=None, icky_
         dp("sampleYs", sizes(lambdas,lmean,lstar,ll,gammas,base_logits,ldajs))
         ldajsums = ldajsums + ldajs.squeeze()
         YSums = YSums + ys
+        meanYSums = meanYSums + meanys
 
         nrbyc = data.ns[u].view(1,R,1) #used as weights for running variance calculation
         qs = ys / nrbyc
@@ -1431,7 +1442,7 @@ def sampleYs(fit,data,n,previousSamps = None,weightToUndo=1.,indices=None, icky_
 
     denses = torch.stack([guidesampdenses,ldajsums,moddenses,modnps],1)
     dp("denses",sizes(denses))
-    return [t.clone().detach().requires_grad_(False) for t in (gammas,YSums, denses,Qvarcounters)]
+    return [t.clone().detach().requires_grad_(False) for t in (gammas,YSums, meanYSums,denses,Qvarcounters)]
 
 def saveYsamps(samps, data, subsample_n, nparticles,nsteps,dversion="",filebase=FILEBASE + "funnyname_",i=None,N=None):
 